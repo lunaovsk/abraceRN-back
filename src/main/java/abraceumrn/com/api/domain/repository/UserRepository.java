308773327ac02ab1914f31ec04ef08140d6080a7
Matheus Luna <matheusluna151634@gmail.com>
2026-05-08 10:12:21 -0300

    feat(repository): add UserRepository

--- src/main/java/abraceumrn/com/api/domain/repository/UserRepository.java
+++ src/main/java/abraceumrn/com/api/domain/repository/UserRepository.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserData, String>{
+public interface UserRepository extends JpaRepository<UserData, Long>{
+
     Optional<UserData> findByUsername(String username);
 
+    boolean existsByUsername(String username);
 }
