27065620c77cb8315da2506c431358bca7aa2b7b
Matheus Luna <matheusluna151634@gmail.com>
2026-05-08 10:12:14 -0300

    feat(user): add UserData entity

--- src/main/java/abraceumrn/com/api/domain/user/UserData.java
+++ src/main/java/abraceumrn/com/api/domain/user/UserData.java
@@ -1,31 +1,34 @@
 package abraceumrn.com.api.domain.user;
 
 
+import abraceumrn.com.api.domain.dto.UserDTO;
 import abraceumrn.com.api.domain.enumItem.Role;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Table(name = "user")
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserData {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String username;
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
 
-
-    public UserData () {
-
-    }
-    public UserData (UserDTO userDTO) {
-        this.username = userDTO.username();
-        this.password = userDTO.password();
-        this.role = userDTO.role();
+    public UserData (String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
     }
 
 }
