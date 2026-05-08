de2947c7766bd114932fe1b99d715705f0ae04ae
Matheus Luna <matheusluna151634@gmail.com>
2026-05-08 10:20:57 -0300

    chore(swagger): update SwaggerConfig

--- src/main/java/abraceumrn/com/api/infra/security/SwaggerConfig.java
+++ src/main/java/abraceumrn/com/api/infra/security/SwaggerConfig.java
@@ -12,7 +12,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("API Dashboard Abrace RN")
-                        .version("1.0")
+                        .version("1.1")
                         .description("Documentação da API de gestão de estoque desenvolvida para a ONG que auxilia mulheres em situação de vulnerabilidade no pós-parto, garantindo a distribuição organizada de itens essenciais para recém-nascidos.")
                 );
     }
