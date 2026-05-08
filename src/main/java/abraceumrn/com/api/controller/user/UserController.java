f15daa4ab67ecf115bbb80d6022b881e552c8642
Matheus Luna <matheusluna151634@gmail.com>
2026-05-08 13:23:50 -0300

    feat(auth): implement login endpoint to authenticate users and return JWT

--- src/main/java/abraceumrn/com/api/controller/user/UserController.java
+++ src/main/java/abraceumrn/com/api/controller/user/UserController.java
@@ -1,12 +1,19 @@
 package abraceumrn.com.api.controller.user;
 
 import abraceumrn.com.api.domain.dto.UserDTO;
+import abraceumrn.com.api.domain.dto.UserRequestDTO;
 import abraceumrn.com.api.domain.dto.UserResponse;
+import abraceumrn.com.api.domain.user.UserData;
+import abraceumrn.com.api.infra.security.TokenJWT;
+import abraceumrn.com.api.infra.security.TokenService;
 import abraceumrn.com.api.infra.security.UserService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +24,15 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/login")
+@SecurityRequirement(name = "bearer-key")
 public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private AuthenticationManager manager;
+    @Autowired
+    private TokenService tokenService;
 
 
     @PostMapping("/create")
@@ -29,11 +41,11 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new UserResponse(account.getUsername(), account.getRole()));
     }
 
-
-
-
-
-
-
-    
+    @PostMapping
+    public ResponseEntity<TokenJWT> login(@RequestBody @Valid UserRequestDTO requestDTO) {
+        var credentials = new UsernamePasswordAuthenticationToken(requestDTO.username(), requestDTO.password());
+        var authenticated = manager.authenticate(credentials);
+        var token = tokenService.generateToken(authenticated);
+        return ResponseEntity.ok().body(new TokenJWT(token));
+    }
 }
