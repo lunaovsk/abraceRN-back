8d9112a8d7ec9e1ac6c85dae5a6365ce9c84f0fc
Matheus Luna <matheusluna151634@gmail.com>
2026-05-09 15:16:32 -0300

    fix(exception): add handler for BadCredentialsException

--- src/main/java/abraceumrn/com/api/infra/exception/ErrorHandling.java
+++ src/main/java/abraceumrn/com/api/infra/exception/ErrorHandling.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -56,6 +57,11 @@ public class ErrorHandling {
     public ResponseEntity<ErrorResponse> handleBadJson(HttpMessageNotReadableException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse("BAD_REQUEST", "Dados preenchido incorretamente.", null));
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredentials(BadCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("BAD_CREDENTIALS", "E-mail ou senha inválidos.", null));
     }
 
     private record ErrorResponse(String errorCode, String error, Object details) {}
